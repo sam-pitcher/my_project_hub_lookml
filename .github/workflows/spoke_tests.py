4ee8296a7bebc0ae6a5cfd37f12f55ba24d02e6c
Sam Pitcher <sampitcher@google.com>
2024-09-17 11:31:56 +0200

    comment

--- .github/workflows/spoke_tests.py
+++ .github/workflows/spoke_tests.py
@@ -9,27 +9,27 @@ print(F"Current git branch: {git_branch}")
 
 
 # env variables have been declared in the yaml file
-instance = 'test.looker.shredr.xyz/'  # host name e.g. 'mydomain.looker.com'
+# instance = 'test.looker.shredr.xyz/'  # host name e.g. 'mydomain.looker.com'
 
-sdk = looker_sdk.init40()
+# sdk = looker_sdk.init40()
 
-spoke_project_id="my_project_spoke"
-hub_project_id="my_project_hub"
+# spoke_project_id="my_project_spoke"
+# hub_project_id="my_project_hub"
 
 # briefly deploy new code to production in dev instance
-sdk.deploy_ref_to_production(project_id=hub_project_id, branch=git_branch)
+# sdk.deploy_ref_to_production(project_id=hub_project_id, branch=git_branch)
 
 # change to dev mode in dev instance
-body = {"workspace_id": "dev"}
-sdk.update_session(body)
+# body = {"workspace_id": "dev"}
+# sdk.update_session(body)
 
 # update dependancies in spoke project with new deployed code from hub
-sdk.lock_all(spoke_project_id)
+# sdk.lock_all(spoke_project_id)
 
 # validate the code in spoke didn't break
-project_validation = sdk.validate_project(spoke_project_id)
-print(f"Errors: {project_validation.errors}")
-print(f"Models not validated: {project_validation.models_not_validated}")
+# project_validation = sdk.validate_project(spoke_project_id)
+# print(f"Errors: {project_validation.errors}")
+# print(f"Models not validated: {project_validation.models_not_validated}")
 
 # revert back to master branch in hub
 # sdk.deploy_ref_to_production(project_id=hub_project_id, branch='master')
